7cf6949e0666c4a5d13881185f58b6e26cbda06d
TrueIB <18538252970@163.com>
2026-01-14 10:08:33 +0000

    update dataset_generation.py.
    Signed-off-by: TrueIB <18538252970@163.com>

--- dataset_generation.py
+++ dataset_generation.py
@@ -1,8 +1,26 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
+import random
+import numpy
+import os
 
 
 if __name__ == '__main__':
-    image = Image.new('RGB', (1000, 1000), color='white')
-    for i in range(19968, 40944):
-        print(i, chr(i))
+    WIDTH, HEIGHT = 10000, 10000
+    image = Image.new('L', (WIDTH, HEIGHT), color=255)
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype('c:\windows\fonts\simsun.ttc', size=60)
+    characters = []
+    IMAGE_WIDTH = 100
+    for i in range(19968, 40870): 
+        characters.append(chr(i))
+        print('\r', i, '汉字：', chr(i), '已添加成功', end='')
+    for i in range(0, HEIGHT, 100):
+        for j in range(0, WIDTH, 100):
+            draw.text((i, j), random.choice(characters), font=font, fill='black')
+    load = image.load()
+    for i in range(HEIGHT):
+        for j in range(WIDTH):
+            load[i, j] = 0 if load[i, j] < 50 else 255
+    #os.system('pause')
+    image.save('dataset_example.png')
     image.show()
